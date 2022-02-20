d9a133cd639019fc7ca3ba32c7ede4aa8e8bed95
Vanessa Costa <vanessapaixao@cjr.org.br>
2022-02-20 18:57:22 -0300

    feat(main): adiciona classe duracao a funcao main

--- main.cpp
+++ main.cpp
@@ -4,20 +4,38 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     TUCidade testeA;
-    switch(testeA.run()) {
-        case TUCidade::SUCESSO: cout << "SUCESSO - CIDADE" << endl;
-            break;
-        case TUCidade::FALHA: cout << "FALHA - CIDADE" << endl;
-            break;
+    switch (testeA.run())
+    {
+    case TUCidade::SUCESSO:
+        cout << "SUCESSO - CIDADE" << endl;
+        break;
+    case TUCidade::FALHA:
+        cout << "FALHA - CIDADE" << endl;
+        break;
     };
 
     TUCodigo testeB;
-    switch(testeB.run()) {
-        case TUCodigo::SUCESSO: cout << "SUCESSO - CODIGO" << endl;
-            break;
-        case TUCodigo::FALHA: cout << "FALHA - CODIGO" << endl;
-            break;
+    switch (testeB.run())
+    {
+    case TUCodigo::SUCESSO:
+        cout << "SUCESSO - CODIGO" << endl;
+        break;
+    case TUCodigo::FALHA:
+        cout << "FALHA - CODIGO" << endl;
+        break;
+    };
+
+    TUDuracao testeC;
+    switch (testeC.run())
+    {
+    case TUDuracao::SUCESSO:
+        cout << "SUCESSO - DURACAO" << endl;
+        break;
+    case TUDuracao::FALHA:
+        cout << "FALHA - DURACAO" << endl;
+        break;
     };
 };
