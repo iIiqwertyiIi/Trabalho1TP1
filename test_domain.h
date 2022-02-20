d33b1ad90f5d2faba94edf806b5fa00df5358b10
Vanessa Costa <vanessapaixao@cjr.org.br>
2022-02-20 18:56:14 -0300

    feat(testes): criar testes para a classe duracao

--- test_domain.h
+++ test_domain.h
@@ -5,44 +5,66 @@
 
 using namespace std;
 
-class TUCidade {
-    private:
-        inline static const string CIDADE_VALIDA = "Istambul";
-        inline static const string CIDADE_INVALIDA = "São Paulo";
-        Cidade *cidade;
-        int estado;
-        void setUp();
-        void tearDown();
-        void testarValido();
-        void testarInvalido();
-    public:
-        static const int SUCESSO = 1;
-        static const int FALHA = 0;
-        int run();
+class TUCidade
+{
+private:
+    inline static const string CIDADE_VALIDA = "Istambul";
+    inline static const string CIDADE_INVALIDA = "São Paulo";
+    Cidade *cidade;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarValido();
+    void testarInvalido();
+
+public:
+    static const int SUCESSO = 1;
+    static const int FALHA = 0;
+    int run();
 };
 
-class TUCodigo {
-    private:
-        inline static const string CODIGO_VALIDO = "2586719";
-        inline static const string CODIGO_INVALIDO_1 = "0000000";
-        inline static const string CODIGO_INVALIDO_2 = "2586712";
-        inline static const string CODIGO_INVALIDO_3 = "ASCII34";
-        inline static const string CODIGO_INVALIDO_4 = "258671";
-        inline static const string CODIGO_INVALIDO_5 = "258671938";
-        Codigo *codigo;
-        int estado;
-        void setUp();
-        void tearDown();
-        void testarValido();
-        void testarInvalido1();
-        void testarInvalido2();
-        void testarInvalido3();
-        void testarInvalido4();
-        void testarInvalido5();
-    public:
-        static const int SUCESSO = 1;
-        static const int FALHA = 0;
-        int run();
+class TUCodigo
+{
+private:
+    inline static const string CODIGO_VALIDO = "2586719";
+    inline static const string CODIGO_INVALIDO_1 = "0000000";
+    inline static const string CODIGO_INVALIDO_2 = "2586712";
+    inline static const string CODIGO_INVALIDO_3 = "ASCII34";
+    inline static const string CODIGO_INVALIDO_4 = "258671";
+    inline static const string CODIGO_INVALIDO_5 = "258671938";
+    Codigo *codigo;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarValido();
+    void testarInvalido1();
+    void testarInvalido2();
+    void testarInvalido3();
+    void testarInvalido4();
+    void testarInvalido5();
+
+public:
+    static const int SUCESSO = 1;
+    static const int FALHA = 0;
+    int run();
+};
+
+class TUDuracao
+{
+private:
+    inline static const int DURACAO_VALIDA = 30;
+    inline static const int DURACAO_INVALIDA = 45;
+    Duracao *duracao;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarValido();
+    void testarInvalido();
+
+public:
+    static const int SUCESSO = 1;
+    static const int FALHA = 0;
+    int run();
 };
 
 #endif
