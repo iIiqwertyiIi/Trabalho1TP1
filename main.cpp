aa4a9e08bb109b3ef35256f78d4582ad5169f748
Nícolas Paulin Benatto <nicpabe@gmail.com>
2022-02-23 14:13:55 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,31 +7,25 @@ using namespace std;
 int main() {
     TUCidade testeA;
     switch (testeA.run()) {
-        case TUCidade::SUCESSO:
-            cout << "SUCESSO - CIDADE" << endl;
+        case TUCidade::SUCESSO: cout << "SUCESSO - CIDADE" << endl;
             break;
-        case TUCidade::FALHA:
-            cout << "FALHA - CIDADE" << endl;
+        case TUCidade::FALHA: cout << "FALHA - CIDADE" << endl;
             break;
     };
 
     TUCodigo testeB;
     switch (testeB.run()) {
-        case TUCodigo::SUCESSO:
-            cout << "SUCESSO - CODIGO" << endl;
+        case TUCodigo::SUCESSO: cout << "SUCESSO - CODIGO" << endl;
             break;
-        case TUCodigo::FALHA:
-            cout << "FALHA - CODIGO" << endl;
+        case TUCodigo::FALHA: cout << "FALHA - CODIGO" << endl;
             break;
     };
 
     TUDuracao testeDuracao;
     switch (testeDuracao.run()) {
-        case TUDuracao::SUCESSO:
-            cout << "SUCESSO - DURACAO" << endl;
+        case TUDuracao::SUCESSO: cout << "SUCESSO - DURACAO" << endl;
             break;
-        case TUDuracao::FALHA:
-            cout << "FALHA - DURACAO" << endl;
+        case TUDuracao::FALHA: cout << "FALHA - DURACAO" << endl;
             break;
     };
 };
