99b05a04f3107ac8f4cd2745ec57ab4efd1f8ea3
Nícolas Paulin Benatto <nicpabe@gmail.com>
2022-02-23 14:12:39 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,38 +4,34 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     TUCidade testeA;
-    switch (testeA.run())
-    {
-    case TUCidade::SUCESSO:
-        cout << "SUCESSO - CIDADE" << endl;
-        break;
-    case TUCidade::FALHA:
-        cout << "FALHA - CIDADE" << endl;
-        break;
+    switch (testeA.run()) {
+        case TUCidade::SUCESSO:
+            cout << "SUCESSO - CIDADE" << endl;
+            break;
+        case TUCidade::FALHA:
+            cout << "FALHA - CIDADE" << endl;
+            break;
     };
 
     TUCodigo testeB;
-    switch (testeB.run())
-    {
-    case TUCodigo::SUCESSO:
-        cout << "SUCESSO - CODIGO" << endl;
-        break;
-    case TUCodigo::FALHA:
-        cout << "FALHA - CODIGO" << endl;
-        break;
+    switch (testeB.run()) {
+        case TUCodigo::SUCESSO:
+            cout << "SUCESSO - CODIGO" << endl;
+            break;
+        case TUCodigo::FALHA:
+            cout << "FALHA - CODIGO" << endl;
+            break;
     };
 
     TUDuracao testeDuracao;
-    switch (testeDuracao.run())
-    {
-    case TUDuracao::SUCESSO:
-        cout << "SUCESSO - DURACAO" << endl;
-        break;
-    case TUDuracao::FALHA:
-        cout << "FALHA - DURACAO" << endl;
-        break;
+    switch (testeDuracao.run()) {
+        case TUDuracao::SUCESSO:
+            cout << "SUCESSO - DURACAO" << endl;
+            break;
+        case TUDuracao::FALHA:
+            cout << "FALHA - DURACAO" << endl;
+            break;
     };
 };
