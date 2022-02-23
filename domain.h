347898bfa014110cc800122601840d8ec36272ac
Nícolas Paulin Benatto <nicpabe@gmail.com>
2022-02-23 14:10:56 -0300

    Update domain.h

--- domain.h
+++ domain.h
@@ -7,59 +7,50 @@
 
 using namespace std;
 
-class Cidade
-{
-private:
-    inline static const vector<string> CIDADES = {
-        "Hong Kong", "Bangkok", "Macau", "Singapura",
-        "Londres", "Paris", "Dubai", "Delhi", "Istambul",
-        "Kuala Lumpur", "Nova Iorque", "Antalya", "Mumbai",
-        "Shenzhen", "Phuket"};
-    string cidade;
-    void validar(string);
-
-public:
-    void setCidade(string);
-    string getCidade() const;
+class Cidade {
+    private:
+        inline static const vector<string> CIDADES = {
+            "Hong Kong", "Bangkok", "Macau", "Singapura",
+            "Londres", "Paris", "Dubai", "Delhi", "Istambul",
+            "Kuala Lumpur", "Nova Iorque", "Antalya", "Mumbai",
+            "Shenzhen", "Phuket"};
+        string cidade;
+        void validar(string);
+    public:
+        void setCidade(string);
+        string getCidade() const;
 };
 
-inline string Cidade::getCidade() const
-{
+inline string Cidade::getCidade() const {
     return cidade;
 };
 
-class Codigo
-{
-private:
-    inline static const string INVALIDO = "0000000";
-    string codigo;
-    void validar(string);
-
-public:
-    void setCodigo(string);
-    string getCodigo() const;
+class Codigo {
+    private:
+        inline static const string INVALIDO = "0000000";
+        string codigo;
+        void validar(string);
+    public:
+        void setCodigo(string);
+        string getCodigo() const;
 };
 
-inline string Codigo::getCodigo() const
-{
+inline string Codigo::getCodigo() const {
     return codigo;
-}
-
-class Duracao
-{
-private:
-    inline static const vector<int> DURACOES = {
-        30, 60, 90, 120, 180};
-    int duracao;
-    void validar(int);
-
-public:
-    void setDuracao(int);
-    int getDuracao() const;
 };
 
-inline int Duracao::getDuracao() const
-{
+class Duracao {
+    private:
+        inline static const vector<int> DURACOES = {
+            30, 60, 90, 120, 180};
+        int duracao;
+        void validar(int);
+    public:
+        void setDuracao(int);
+        int getDuracao() const;
+};
+
+inline int Duracao::getDuracao() const {
     return duracao;
 };
 
